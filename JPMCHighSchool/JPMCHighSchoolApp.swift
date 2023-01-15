a4be761bd3af8c74f571fbf2fa24bee641085bc2
aMalhotra19 <anjumalhotra1989@gmail.com>
2023-01-14 22:48:16 -0800

    Initial Commit

---
+++ JPMCHighSchool/JPMCHighSchoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPMCHighSchoolApp.swift
+//  JPMCHighSchool
+//
+//  Created by Anju Malhotra on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct JPMCHighSchoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
