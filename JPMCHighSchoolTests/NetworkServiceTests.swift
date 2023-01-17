da45f5ddc10d62d6c536894ed18ca04d35d7a48a
aMalhotra19 <anjumalhotra1989@gmail.com>
2023-01-16 21:19:12 -0800

    Added test for NetworkService

---
+++ JPMCHighSchoolTests/NetworkServiceTests.swift
@@ -0,0 +1,58 @@
+//
+//  NetworkService.swift
+//  JPMCHighSchoolTests
+//
+//  Created by Anju Malhotra on 1/16/23.
+//
+
+import XCTest
+@testable import JPMCHighSchool
+import Combine
+
+final class NetworkServiceTests: XCTestCase {
+    var networkService: NetworkService!
+    private var cancellables: Set<AnyCancellable>!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        networkService = NetworkService.shared
+        cancellables = []
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        networkService = nil
+        cancellables = nil
+    }
+    
+    func testGetData() throws {
+        let result = networkService.getData(url: URL(string: "https://www.google.com")!)
+        var error: Error?
+        var responseData: Data? = nil
+        let expectation = self.expectation(description: "Expectation")
+        result
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let returnedError):
+                    error = returnedError
+                }
+                
+                // Fullfilling our expectation to unblock
+                // our test execution:
+                expectation.fulfill()
+            } receiveValue: { data in
+                responseData = data
+            }
+            .store(in: &cancellables)
+        
+        waitForExpectations(timeout: 10)
+        
+        // Asserting that our Combine pipeline yielded the
+        // correct output:
+        XCTAssertNil(error)
+        XCTAssertNotNil(responseData)
+    }
+    
+}
